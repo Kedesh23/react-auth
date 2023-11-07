36f17ce91fea5a314c24267e900b9035b53eaf7c
IKISSA PENDY Sevan Kedesh <pendysevan10@gmail.com>
2023-11-07 18:02:03 +0100

    section left done

--- src/assets/Screens/Login.jsx
+++ src/assets/Screens/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Ecole241 from "../../Components/Ecole241";
 import Input from "../../Components/Input";
 import Button from "../../Components/Button";
@@ -7,33 +8,33 @@ export default function Login(){
     return (
         <div className="flex">
             <div className="w-[50%] h-full">
-                <div className=" px-[9em]">
+                <div className=" px-[9em] my-[3em]">
                     <Ecole241/>
                     <h1 className="font-bold text-3xl">Connexion</h1> 
                     <p className="text-gray-500">Saisissez votre email et votre mot de passe pour avoir  acces au Dashboard</p>
                 </div>
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center my-[3em]">
                     <form className="gap-5">
-                        <div className="">
-                            <p className="font-bold">Email</p>
+                        <div className=" my-[3em]">
+                            <p className="font-bold my-[1em]">Email</p>
                             <Input type = "text" placeholder = "Robert@gmail.com" className="bg-[#EEF2FF] border-[#EEF2FF] border-transparentm h-[5em] w-[35em] outline-none"/>
                         </div>
                         <div>
-                            <div className="flex justify-between">
+                            <div className="flex justify-between my-[1em]">
                                 <p className="font-bold">Mot de passe</p>
-                                <p className="font-bold text-[#3D37A3]">Mot de passe oublie?</p>
+                                <Link to="/Reset" className="font-bold text-[#3D37A3]">Mot de passe oublie?</Link>
                             </div>
                             <Input type = "password" placeholder = "Mot de passe" className="bg-[#EEF2FF] border-[#EEF2FF] border-transparentm h-[5em] w-[35em] outline-none"/>
                         </div>
-                        <div className="flex gap-5 items-center">
+                        <div className="flex gap-5 items-center my-[3em]">
                             <div className="w-[3em] h-[3em] bg-[#EEF2FF]"></div>
                             <p className="font-bold text-xl">Se souvenir de moi</p>
                         </div>
                         <Button className="bg-[#3D37A3] text-white font-bold text-center px-[11.4em] py-[1.5em] rounded-lg text-xl" title="Connexion"/>
                     </form>
-                    <div className="flex justify-between items-center my-[3em]">
-                        <p className="text-gray-[400] text-lg">Vous n'avez pas un compte?</p>
-                        <p className="bg-[#3D37A3] text-lg">C'est gratuit !</p>
+                    <div className="flex justify-between items-center my-[3em] font-bold text-2xl">
+                        <p className="text-[#BCBFC5]">Vous n'avez pas un compte?</p>
+                        <p className="text-[#3D37A3]">C'est gratuit !</p>
                     </div>
                 </div>
             </div>
